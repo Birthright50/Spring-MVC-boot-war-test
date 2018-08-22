9139eed840ce267225470f76bf6e82b552efc1ed
Kozlovsky Petr <p.kozlovsky@bpcbt.com>
2018-08-22 17:13:36 +0300

    initial commit

---
+++ src/main/java/com/birthright/demotld/DemoTldApplication.java
@@ -0,0 +1,12 @@
+package com.birthright.demotld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTldApplication.class, args);
+    }
+}
---
+++ src/main/resources/templates/home.ftl
@@ -0,0 +1,13 @@
+<#assign form=JspTaglibs["http://www.springframework.org/tags/form"]/>
+<#assign spring=JspTaglibs["http://www.springframework.org/tags"]/>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
